4f3ce6de3e36259cbc63c6313ce3a1d6fae79c07
SL4MER <nekid2007@gmail.com>
2025-10-24 17:55:58 +0300

    создал проверку ввода и закомитил

--- krestikinoliki/krestikinoliki.cpp
+++ krestikinoliki/krestikinoliki.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int Inputcorrectness(char a, int* i, int x, int y) {
+	if (a == 'x' || a == '0'){
+		cout << "\nПРЕДУПРЕЖДЕНИЕ: Данное поле уже занято, выберите другое\n";
+		return (*i)--;
+	}
+}
+
 void PrintArr(char a[3][3]) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -26,6 +33,7 @@ int main() {
 		cin >> x;
 		cout << "Введите номер столбца: ";
 		cin >> y;
+		Inputcorrectness(a[x - 1][y - 1], &i, x, y);
 		cout << endl;
 		a[x - 1][y - 1] = (i % 2 == 0) ? X : O;
 		PrintArr(a);
